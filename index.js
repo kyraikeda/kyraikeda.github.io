359928aec8cfe247172336796793f7d5c7ccc2bd
kyraikeda <kyraikeda@gmail.com>
2023-07-31 16:09:46 -1000

    major fix to projects section! also fixed "to top" scroll button functionality yay

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // FUNCTIONS!
 
+// once document is loaded, check scroll position constantly
 $(document).ready(function () {
     if (document.body.classList.contains('index')) {
         // CONTROL SCROLL TO TOP FUNCTIONALITY
@@ -10,16 +11,22 @@ $(document).ready(function () {
 // Click image to view full screen function but idk if i want this
 // $(document).ready(function(){$("img").click(function(){this.requestFullscreen()})});
 
+// show/hide "back to top' button
+function showScroll() {
+    if (document.documentElement.scrollTop > 50) {
+        document.getElementById("to-top-button").setAttribute("data-reveal", "");
+        document.getElementById("to-top-button").removeAttribute("data-hide", "");
+    }
 
-function showScroll() { // show/hide "back to top' button
-    (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) ?
-        document.getElementById("toTopButton").hidden = false :
-        document.getElementById("toTopButton").hidden = true
+    if (document.documentElement.scrollTop === 0) {
+        document.getElementById("to-top-button").setAttribute("data-hide", "");
+        document.getElementById("to-top-button").removeAttribute("data-reveal", "");
+    }
 }
 
 function backToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
-    window.location.href="#"; // change URL fragment to # when scroll back to top
+    window.location.href = "#"; // change URL fragment to # when scroll back to top
 }
 
 // AFTER CLICKING ON NAVBAR TAB, SCROLL TO THAT SECTION
@@ -32,23 +39,25 @@ function scrollToView(id) {
     })
 };
 
-// CONTROL "EXPERIENCE" TAB VIEW
-// function openExp(event, idName) {
-//     let tabButtons, tabContents;
+// MAKE SLIDER WORK FOR PROJECTS
+const buttons = document.querySelectorAll("button.slider-arrow"); // find buttons first
+// add event listener to each button on click
+buttons.forEach(button => button.addEventListener("click", function () {
+    changeProjectSlide(button);
+}));
 
-//     // hide all tab content at first
-//     tabContents = document.getElementsByClassName("tabContent");
-//     for (let i = 0; i < tabContents.length; i++) {
-//         tabContents[i].hidden = true;
-//     }
-
-//     // remove any tab with "active" class
-//     tabButtons = document.getElementsByClassName("tablinks");
-//     for (let i = 0; i < tabButtons.length; i++) {
-//         tabButtons[i].classList.remove('active');
-//     }
-
-//     // show the current tab and add "active" class
-//     let currentTab = document.getElementById(idName);
-//     currentTab.hidden = false;
-// }
+function changeProjectSlide(button) {
+    // get NodeList of each project slide
+    slidesList = document.querySelectorAll("div.proj-card");
+    currentSlide = document.querySelector("div.proj-card-container").querySelector("[data-active]");
+    // previous button = -1 to go back, next button = 1 to go forward in the array
+    offset = (button.dataset.previous === "") ? -1 : 1;
+    currentIndex = [...slidesList].findIndex(node => node.dataset.active === "");
+    newIndex = currentIndex + offset;
+    // handles reaching ends of array
+    if (newIndex < 0) newIndex = slidesList.length - 1;
+    if (newIndex >= slidesList.length) newIndex = 0;
+    // gives new slide data-active attribute to make visible and removes it from current slide
+    slidesList.item(newIndex).setAttribute("data-active", "");
+    currentSlide.removeAttribute("data-active", "");
+};
